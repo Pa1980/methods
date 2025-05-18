7c1d1558455e875911a1bb6e5377038bb8a4780e
Pavel <Pahenmurman@gmail.com>
2025-05-18 14:40:11 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+// hw
         Author oneAuthor = new Author("Александр","Пушкин");
         Author twoAuthor = new Author("Михаил", "Булгаков");
         System.out.println(oneAuthor.getName()+" "+oneAuthor.getSurname());
